c6a8f1a90793aa0cf2aa9aea8f42f1a013e6ce64
tiwoah <corextyy@gmail.com>
2023-10-15 01:31:05 -0400

    v2: better game concept and way more functionality + title screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,96 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 import eye from "./assets/eye.png";
 import eye_gif from "./assets/eye.gif";
 import Game from "./components/Game";
 import sound_boot from "./assets/boot.mp3";
 
+import { easeIn, motion } from "framer-motion";
+
 function App() {
+  const startHint = useRef(null);
+  const [started, setStarted] = useState(false);
   console.log("App rendered");
 
-  function play() {
+  function start() {
+    if (started) return;
     new Audio(sound_boot).play();
+    setStarted(true);
   }
+
+  const variants = {
+    active: {
+      position: "absolute",
+      left: "50%",
+      top: "45%",
+      transform: "translate(-50%, -50%)",
+    },
+    normal: {
+      position: "absolute",
+      left: "50%",
+      top: "10%",
+      transform: "translate(-50%, -0%)",
+    },
+  };
+
+  const variants_overlay = {
+    active: {
+      height: "100vh",
+    },
+    normal: {
+      height: "0vh",
+    },
+  };
+
+  const variants_description = {
+    active: {
+      opacity: 1,
+    },
+    normal: {
+      opacity: 0,
+    },
+  };
 
   return (
     <>
-      <div>
-        <h1>
-          PERCEPTI<img className="eye" src={eye_gif}></img>N
-        </h1>
+      <div className="container" onClick={start}>
+        <motion.div
+          className="overlay"
+          variants={variants_overlay}
+          initial="active"
+          animate={started ? "normal" : "active"}
+          transition={{ duration: 1.5, ease: "easeInOut", delay: 0 }}
+        ></motion.div>
+
+        <motion.div
+          className="center"
+          variants={variants}
+          initial="active"
+          animate={started ? "normal" : "active"}
+          transition={{ duration: 1, ease: "easeInOut", delay: 0 }}
+        >
+          <h1>
+            PERCEPTI<img className="eye" src={eye_gif}></img>N
+          </h1>
+          <motion.h2
+            variants={variants_description}
+            initial="active"
+            animate={started ? "normal" : "active"}
+            transition={{ duration: 1, ease: "easeInOut", delay: 0 }}
+          >
+            A challenging memory game.
+          </motion.h2>
+        </motion.div>
+        <motion.h2
+          className={`start-hint ${started ? "animation-stopped" : ""}`}
+          ref={startHint}
+          variants={variants_description}
+          initial="active"
+          animate={started ? "normal" : "active"}
+          transition={{ duration: 0.2, ease: "easeInOut" }}
+        >
+          {/* Click anywhere to begin */}
+        </motion.h2>
         <Game />
       </div>
     </>
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,104 +8,143 @@ export default function Game() {
   console.log("Game component rendered");
 
   let newest = null;
+  let dead = true;
+  let i = 0;
+  const [highscore, setHighscore] = useState(0);
 
   function timeout(delay) {
     return new Promise((res) => setTimeout(res, delay));
   }
 
-  const handleNextRound = async () => {
+  const handleNextRound = async (isFirst) => {
     var classes = document.querySelectorAll(".item:not(.selected)");
+
+    classes.forEach(function (item) {
+      item.classList.add("selected");
+    });
+
+    isFirst ? null : await timeout(400);
+
+    classes.forEach(function (item) {
+      item.classList.remove("selected");
+    });
+
     var keys = Object.keys(classes);
     newest = classes[keys[(keys.length * Math.random()) << 0]];
 
-    await timeout(200);
     new Audio(sound_boop).play();
     newest.classList.add("selected");
   };
 
-  const handleClick = (event) => {
+  const handleClick = async (event) => {
+    if (dead) return;
     if (event.target.classList.contains("item")) {
       if (event.target == newest) {
-        console.log("W, going next round.");
         handleNextRound();
+        i += 1;
       } else {
-        console.log("L");
+        dead = true;
+        if (i > highscore) {
+          setHighscore(i);
+          console.log(highscore);
+        }
         new Audio(sound_loss).play();
+        var items = document.querySelectorAll(".item");
+
+        items.forEach(function (item) {
+          item.classList.add("dead");
+        });
+
+        await timeout(250);
+
+        items.forEach(function (item) {
+          item.classList.remove("dead");
+        });
+
+        newest.classList.add("reveal");
       }
     }
   };
 
   const handleStart = async (event) => {
-    console.log("start");
+    dead = false;
+    i = 0;
+    var classes = document.querySelectorAll(".reveal");
+
+    classes.forEach(function (item) {
+      item.classList.remove("reveal");
+    });
 
     var items = document.querySelectorAll(".item");
 
-    // Loop through each element and remove class "selected" if it exists
     items.forEach(function (item) {
       if (item.classList.contains("selected")) {
         item.classList.remove("selected");
       }
     });
 
-    handleNextRound();
+    handleNextRound(true);
   };
 
   return (
     <>
-      <div className="container" onClick={handleClick}>
-        <div className="group group-1">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-2">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-3">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-4">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-5">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-6">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-7">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-8">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-        </div>
-        <div className="group group-9">
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
-          <div className="item item-x"></div>
+      <span style={{ color: "#FFF" }}>Best score: {highscore}</span>
+      <div className="game-container">
+        <div className="game" onMouseDown={handleClick}>
+          <div className="group group-1">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-2">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-3">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-4">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-5">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-6">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-7">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-8">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
+          <div className="group group-9">
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+            <div className="item item-x"></div>
+          </div>
         </div>
       </div>
       <Start handleStart={handleStart}></Start>
